da98f8ae5254be2de1c2467c0a1cb031a792a8a9
Shivam Priyadarshi <shivprishi@gmail.com>
2022-04-20 07:00:58 +0000

    Finish custom filters

---
+++ blog/templatetags/blog_extras.py
@@ -0,0 +1,35 @@
+from django import template
+from django.utils.html import escape
+from django.utils.safestring import mark_safe
+from django.utils.html import format_html
+from django.contrib.auth import get_user_model
+user_model = get_user_model()
+
+register=template.Library()
+
+
+@register.filter
+def author_details(author, current_user):
+    if not isinstance(author, user_model):
+        # return empty string as safe default
+        return ""
+
+    if author==current_user:
+        name=format_html("<strong>Me</strong>")
+    elif author.first_name and author.last_name:
+        name = escape(f"{author.first_name} {author.last_name}")
+    else:
+        name = escape(f"{author.username}")
+    
+    if author.email:
+      #email = escape(author.email)
+      #prefix = f'<a href="mailto:{email}">'
+      #suffix = "</a>"
+      prefix = format_html('<a href="mailto:{}">', author.email)
+      suffix = format_html("</a>")
+
+    else:
+        prefix = ""
+        suffix = ""
+
+    return format_html('{}{}{}', prefix, name, suffix)
